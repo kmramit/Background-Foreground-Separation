6c6f0a5c6a55927b2011f673bb0514da36c3f9e1
Sundararajan R <dyoucme@gmail.com>
2016-04-07 18:50:05 +0530

    Added layered model detection

--- cb/CBDriver.cpp
+++ cb/CBDriver.cpp
@@ -6,10 +6,17 @@ using namespace cv;
 
 int temporal_bound, algo_phase, num_frames;
 
-int main()
+int main(int argc, char* argv[])
 {
     /* Provide your own pathname */
-    VideoCapture cap("/home/rajan/Courses/CS771/videos/input_video_sample3.mov");
+    //VideoCapture cap("../../videos/input_video_sample1.mov");
+    if (argc != 2) {
+	 cerr <<"Incorrect input" << endl;
+	 cerr <<"exiting..." << endl;
+	 return EXIT_FAILURE;
+    }
+
+    VideoCapture cap(argv[1]);
 
     if(!cap.isOpened())
         return -1;
@@ -46,14 +53,14 @@ int main()
                 blue = (int) intensity.val[0];
                 green = (int) intensity.val[1];
                 red = (int) intensity.val[2];
-                codebooks[j][i].FindMatch(red, green, blue, time);
+                codebooks[j][i].FindMatch(0, red, green, blue, time);
                 /* codebooks[j][i].FindMatch(intensity.val[2], intensity.val[1], intensity.val[0], time); */
 
             }
         }
 
         time++;
-        if(time == 900)
+        if(time == 500)
             break;
     }
     //cap.release();
@@ -68,20 +75,20 @@ int main()
 
     for(j=0;j<480;j++) {
         for(i=0;i<640;i++) {
-            codebooks[j][i].WrapAround();
-            sum += codebooks[j][i].Length();
+            codebooks[j][i].WrapAround(0);
+            sum += codebooks[j][i].Length(0);
         }
     }
 
     printf("Average codewords per codebook = %f\n",(sum*1.0)/(480*640));
 
-    temporal_bound = num_frames/2;
+    temporal_bound = 9*num_frames/10;
 
     sum = 0;
     for(j=0;j<480;j++) {
         for(i=0;i<640;i++) {
-            codebooks[j][i].TemporalFit();
-            sum += codebooks[j][i].Length();
+            codebooks[j][i].TemporalFit(0);
+            sum += codebooks[j][i].Length(0);
         }
     }
     printf("Average codewords per codebook (after temporal fitting) = %f\n",(sum*1.0)/(480*640));
@@ -92,7 +99,8 @@ int main()
 
     Mat frame3(480, 640, CV_8UC1);
     namedWindow("original",1);
-    namedWindow("classified",1);
+    namedWindow( "Components", 1 );
+    //namedWindow("classified",1);
     for(;;)
     {
         ret = cap.read(frame);
@@ -108,17 +116,76 @@ int main()
                 blue = (int) intensity.val[0];
                 green = (int) intensity.val[1];
                 red = (int) intensity.val[2];
-                if(codebooks[j][i].DetectForeground(red, green, blue, time) == FOREGROUND)
-                    frame3.at<uchar>(j, i) = 255;
+                //if((codebooks[j][i].DetectForeground(0, red, green, blue, time) == BACKGROUND) || 
+		//	(codebooks[j][i].DetectForeground(1, red, green, blue, time) == BACKGROUND))
+                if(codebooks[j][i].DetectForeground(0, red, green, blue, time) == FOREGROUND) {
+		    if(codebooks[j][i].DetectForeground(1, red, green, blue, time) == BACKGROUND)
+			frame3.at<uchar>(j, i) = 120;
+		    else
+                    	frame3.at<uchar>(j, i) = 255;
+		}
                 else
                     frame3.at<uchar>(j, i) = 0;
             }
         }
+        //imshow("classified",frame3);
+
+	Mat dst = Mat::zeros(frame3.rows, frame3.cols, CV_8UC1);
+	Mat dst1 = Mat::zeros(frame3.rows, frame3.cols, CV_8UC1);
+	Mat dst2 = Mat::zeros(frame3.rows, frame3.cols, CV_8UC1);
+	Mat dst3 = Mat::zeros(frame3.rows, frame3.cols, CV_8UC1);
+	Mat element = getStructuringElement( MORPH_RECT, Size( 5,5 ), Point( -1,-1 ) );
+	//medianBlur(frame3, frame3, 3); 
+	GaussianBlur(frame3, dst, Size(21,21), 0, 0);
+	morphologyEx( dst, dst1, MORPH_CLOSE, element );
+	threshold(dst1, dst2, 70, 255, THRESH_BINARY);
+	erode( dst2, dst3, element );
+	imshow( "Components", dst3);
+	//dilate(dst1, dst3, element);
+
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+
+		findContours( dst3, contours, hierarchy,
+			CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE );
+
+		vector<vector<Point> > contours_poly( contours.size() );
+		vector<Rect> boundRect( contours.size() );
+
+		RNG rng(12345);
+		int i = 0;
+		for( ; (i >= 0) && (i < contours.size()); i = hierarchy[i][0] )
+		{ 
+			//approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+			//boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+			boundRect[i] = boundingRect( contours[i] );
+
+			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+			//drawContours( dst1, contours_poly, (int)i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+			if (contourArea(contours[i]) >= 200)
+				rectangle( frame2, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+		}
+		//namedWindow( "Contours", WINDOW_AUTOSIZE );
+		//imshow( "Contours", drawing );
+
+		/*
+		// iterate through all the top-level contours,
+		// draw each connected component with its own random color
+		int idx = 0;
+		for( ; (idx >= 0) && (contours.size()>0); idx = hierarchy[idx][0] )
+		{
+			Scalar color( rand()&255, rand()&255, rand()&255 );
+			drawContours( dst, contours, idx, color, CV_FILLED, 8, hierarchy );
+		}
+		*/
+
+
         imshow("original",frame2);
-        imshow("classified",frame3);
         if(waitKey(30) >= 0)
             break;
 
+	if(time % 100 == 0)
+		printf("Frame %d\n", time);
         time++;
     }
     cap.release();
--- cb/libCodeBook.cpp
+++ cb/libCodeBook.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-// Global constants
-float alpha = 0.5, beta = 1.3, ep1 = 7.0, ep2 = 7.0, Gamma = 0.05, rho = 0.05;
+// Global con0.8nts
+float alpha = 0.95, beta = 1.05, ep1 = 8.0, ep2 = 8.0, Gamma = 0.02, rho = 0.04;
 
 // algo_phase = 0 for training and 1 for testing
 extern int temporal_bound, algo_phase, num_frames;
@@ -57,11 +57,13 @@ struct CodeWord
 
         if(I < I_min) {
             I_min = I;
-            I_hi = min(I_hi, I_min/alpha);
+            I_hi = min(beta*I_max, I_min/alpha);
+            I_lo = alpha*I_max;
         }
         if(I > I_max) {
-            I_max = max(I_max, I);
+            I_max = I;
             I_lo = alpha*I_max;
+            I_hi = min(beta*I_max, I_min/alpha);
         }
         freq++;
         mnrl = max(mnrl, (unsigned short)time - last);
@@ -90,18 +92,23 @@ struct CodeWord
     {
         return ((I >= I_lo) && (I <= I_hi));
     };
+
+    bool DetectionBrightness(int r,int g,int b,float I)
+    {
+        return ((I >= 1.0*I_lo) && (I <= I_hi));
+    };
 };
 
 struct CodeBook
 {
-    CodeWord *head;
+    CodeWord *head[2];
 
-    void Init() { head = NULL; };
+    void Init() { head[0] = head[1] = NULL; };
 
-    int Length()
+    int Length(int layer)
     {
         int len = 0;
-        CodeWord *curr = head;
+        CodeWord *curr = head[layer];
         while(curr != NULL) {
             len++;
             curr = curr->next;
@@ -109,82 +116,82 @@ struct CodeBook
         return len;
     };
 
-    void FindMatch(int r,int g,int b,int time)
+    void FindMatch(int layer,int r,int g,int b,int time)
     {
         CodeWord *curr,*prev;
         float I = sqrt(r*r + g*g + b*b);
         
-        if(head == NULL) {
-            Update(NULL, NULL, r, g, b, time);
+        if(head[layer] == NULL) {
+            Update(layer, NULL, NULL, r, g, b, time);
         }
         else {
-            if((head->AdaptiveColorDist(r, g, b, I) <= ep1) && head->Brightness(r, g, b, I)) {
-                Update(NULL, head, r, g, b, time);
+            if((head[layer]->AdaptiveColorDist(r, g, b, I) <= ep1) && head[layer]->Brightness(r, g, b, I)) {
+                Update(layer, NULL, head[layer], r, g, b, time);
                 return;
             }
 
-            if(head->next == NULL) {
-                Update(NULL, NULL, r, g, b, time);
+            if(head[layer]->next == NULL) {
+                Update(layer, NULL, NULL, r, g, b, time);
                 return;
             }
 
-            prev = head;
-            curr = head->next;
+            prev = head[layer];
+            curr = head[layer]->next;
             while(curr != NULL) {
                 if((curr->AdaptiveColorDist(r, g, b, I) <= ep1) && curr->Brightness(r, g, b, I)) {
-                    Update(prev, curr, r, g, b, time);
+                    Update(layer, prev, curr, r, g, b, time);
                     return;
                 }
                 prev = curr;
                 curr = curr->next;
             }
-            Update(NULL, NULL, r, g, b, time);
+            Update(layer, NULL, NULL, r, g, b, time);
         }
     };
 
-    void Update(CodeWord *prev, CodeWord *curr, int r, int g, int b, int time)
+    void Update(int layer,CodeWord *prev, CodeWord *curr, int r, int g, int b, int time)
     {
         if((prev == NULL) && (curr == NULL)) {
             CodeWord *newWord = (CodeWord *)malloc(sizeof(CodeWord));
             newWord->Init(r, g, b, time);
-            newWord->next = head;
-            head = newWord;
+            newWord->next = head[layer];
+            head[layer] = newWord;
         }
         else if(prev == NULL) {
-            assert(head == curr);
+            assert(head[layer] == curr);
             curr->Update(r, g, b, time);
         }
         else {
             CodeWord *curr = prev->next;
             prev->next = curr->next;
-            curr->next = head;
-            head = curr;
-            head->Update(r, g, b, time);
+            curr->next = head[layer];
+            head[layer] = curr;
+            head[layer]->Update(r, g, b, time);
         }
         return;
     };
 
-    void WrapAround() {
+    void WrapAround(int layer) {
         CodeWord *curr;
-        curr = head;
+        curr = head[layer];
         while(curr != NULL) {
             curr->mnrl = max(curr->mnrl, num_frames - curr->last + curr->first - 1);
             curr = curr->next;
         }
     };
 
-    void TemporalFit()
+    void TemporalFit(int layer)
     {
         CodeWord *valid, *curr, *temp;
 
-        assert(head != NULL);
+        assert(head[layer] != NULL);
 
-        // Find the head of the new linked list which we want to construct
-        curr = head;
+        // Find the head[layer] of the new linked list which we want to construct
+        curr = head[layer];
         while((curr != NULL) && (curr->mnrl > temporal_bound)) {
-            head = head->next;
+            head[layer] = head[layer]->next;
             free(curr);
-            curr = head;
+            curr = head[layer];
         }
         if(curr == NULL)
             return;
@@ -208,34 +215,40 @@ struct CodeBook
         }
     };
 
-    pixel_class DetectForeground(int r,int g,int b,int time)
+    pixel_class DetectForeground(int layer,int r,int g,int b,int time)
     {
         CodeWord *curr,*prev;
         float I = sqrt(r*r + g*g + b*b);
         
-        if(head == NULL) {
+        if(head[layer] == NULL) {
+			if(layer == 1)
+            	Update(layer, NULL, NULL, r, g, b, time);
             return FOREGROUND;
         }
         else {
-            if((head->AdaptiveColorDist(r, g, b, I) <= ep2) && head->Brightness(r, g, b, I)) {
-                Update(NULL, head, r, g, b, time);
+            if((head[layer]->AdaptiveColorDist(r, g, b, I) <= ep2) && head[layer]->DetectionBrightness(r, g, b, I)) {
+                Update(layer, NULL, head[layer], r, g, b, time);
                 return BACKGROUND;
             }
 
-            if(head->next == NULL) {
+            if(head[layer]->next == NULL) {
+				if(layer == 1)
+					Update(layer, NULL, NULL, r, g, b, time);
                 return FOREGROUND;
             }
 
-            prev = head;
-            curr = head->next;
+            prev = head[layer];
+            curr = head[layer]->next;
             while(curr != NULL) {
-                if((curr->AdaptiveColorDist(r, g, b, I) <= ep2) && curr->Brightness(r, g, b, I)) {
-                    Update(prev, curr, r, g, b, time);
+                if((curr->AdaptiveColorDist(r, g, b, I) <= ep2) && curr->DetectionBrightness(r, g, b, I)) {
+                    Update(layer, prev, curr, r, g, b, time);
                     return BACKGROUND;
                 }
                 prev = curr;
                 curr = curr->next;
             }
+			if(layer == 1)
+            	Update(layer, NULL, NULL, r, g, b, time);
             return FOREGROUND;
         }
     };
